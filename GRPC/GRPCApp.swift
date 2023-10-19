033804728f13391e44b2beb43fde8fb12c86909b
varunbagga19 <varunbagga19@gmail.com>
2023-10-19 08:26:43 +0530

    Testing

---
+++ GRPC/GRPCApp.swift
@@ -0,0 +1,17 @@
+//
+//  GRPCApp.swift
+//  GRPC
+//
+//  Created by Varun Bagga on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GRPCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
